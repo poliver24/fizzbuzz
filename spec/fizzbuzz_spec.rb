e6f0ed502d77cb34f5c93d85bbace805533cd793
Lila Walker <lilawlker@gmail.com>
2020-02-09 21:20:09 +0000

    Rspec test 7

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -35,3 +35,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns number when not divisible by 3 and 5' do
+    expect(fizzbuzz(19)).to eq 19
+  end
+end
