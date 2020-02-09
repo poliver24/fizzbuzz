591c88c18944c67592d0cf069c534a24a2b01541
Lila Walker <lilawlker@gmail.com>
2020-02-09 20:51:37 +0000

    Restart fizzbuzz rspec test 1

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,43 +1,7 @@
-require_relative '../lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizz" when divisible by 3' do
-    expect(fizzbuzz(6)).to eq 'fizz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "buzz" when divisible by 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when divisible by 3 & 5' do
-    expect(fizzbuzz(30)).to eq 'fizzbuzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns number when not divisible by 3 or 5' do
-    expect(fizzbuzz(19)).to eq 19
   end
 end
