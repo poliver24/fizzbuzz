63f14a1634df273e691952b5f0a69c6f852e5da0
Patrick Oliver <poliver24@hotmail.com>
2020-02-12 20:56:07 +0000

    restart rspec class test 1

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,5 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  else
-    number
+class Integer
+  def fizzbuzz
+    return 'fizz'
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,43 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizz" when divisible by 3' do
-    expect(fizzbuzz(9)).to eq 'fizz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "buzz" when divisible by 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when divisible by 3 and 5' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-end
-
-describe 'fizzbuzz' do
-  it 'returns number when not divisible by 3 and 5' do
-    expect(fizzbuzz(19)).to eq 19
+  it 'returns "Fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
   end
 end
